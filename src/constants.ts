346c7bee3e88bdc8c9292b58c4f90b99a56b2ed4
MarkSaravi <mark.saravi.peerjs@gmail.com>
2020-11-17 19:01:50 +1100

    adding colors

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,19 @@
 export const FIRST_SENTENCE_ID = '';
+export const BgColors = [
+    '#edf2f8',
+    '#b6cae2',
+    '#7fa2cc',
+    '#497ab6',
+    '#335580',
+    '#162437',
+];
+
+export const Colors = [
+    '#696d72',
+    '#4f5358',
+    '#32363b',
+    '#01070e',
+    '#edf2f8',
+    '#edf2f8',
+];
+

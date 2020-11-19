724cbab8b36df810daef6fc113eabd87887ed276
msaravi <msaravi@nine.com.au>
2020-11-20 07:41:00 +1100

    mouse cursor

--- src/components/ZoomableView/index.tsx
+++ src/components/ZoomableView/index.tsx
@@ -1,10 +1,12 @@
 /* eslint-disable import/extensions */
 // eslint-disable-next-line no-use-before-define
 
-import React, { useState } from "react";
-import { ICON_SIZE } from '../../models/constants';
+import React, { useState, useEffect, MouseEvent } from "react";
+import ReactHtmlParser  from 'react-html-parser';
 import type { IZoomable } from '../../models/types';
 import { ZoomIn, ZoomOut } from "../Buttons";
+
+type CursorType = 'zoom-in' | 'zoom-out';
 
 type OwnProps = {
   content: IZoomable;
@@ -12,15 +14,54 @@ type OwnProps = {
 
 type Props = OwnProps;
 
+
+
 const ZoomableView: React.FC<Props> = (props: Props) => {
   const { content } = props;
   const [zoomLevel, setZoomLevel] = useState<number>(0);
-  const maxLevel = content.getMaxLevel();
+  const [cursor, setCursor] = useState<CursorType>('zoom-in');
+
+  const onMouseClicked = (event: MouseEvent) => {
+    if (event.shiftKey) {
+      setZoomLevel(content.zoomOut().level);
+    } else {
+      setZoomLevel(content.zoomIn().level);
+    }
+  };
+
+  const setMouseCursor = (event: MouseEvent) => {
+    if (event.shiftKey && cursor != 'zoom-out') {
+      setCursor('zoom-out');
+    } else if (!event.shiftKey && cursor != 'zoom-in') {
+      setCursor('zoom-in');
+    }
+  }
+
+  const onMouseEntered = (event: MouseEvent) => {
+    setMouseCursor(event);
+  }
+  const onMouseMoved = (event: MouseEvent) => {
+    setMouseCursor(event);
+  }
+
+  useEffect(()=>{
+    console.log('\x1b[34m%s\x1b[0m', `Cursor changed: ${cursor}`);
+  },[cursor])
+
+  useEffect(()=>{
+    console.log('\x1b[34m%s\x1b[0m', `zoomLevel changed: ${zoomLevel}`);
+  },[zoomLevel])
+
   return (
     <div>
-    <div dangerouslySetInnerHTML={{ __html: content.toString().content }} />
-    {zoomLevel < maxLevel && <ZoomIn handler={()=> setZoomLevel(content.zoomIn().level) } />}
-    {zoomLevel > 0 && <ZoomOut handler={()=> setZoomLevel(content.zoomOut().level) } />}
+      <section
+        onClick={onMouseClicked}
+        onMouseEnter={onMouseEntered}
+        onMouseMove={onMouseMoved}
+        style={{cursor}}
+      >
+        {ReactHtmlParser(content.toString().content)}
+      </section>
     </div>
   );
 };

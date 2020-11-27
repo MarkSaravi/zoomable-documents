ba331919ee1571dbc8f7a35199fbb80202e7291b
msaravi <msaravi@nine.com.au>
2020-11-28 09:27:39 +1100

    first level description

--- src/data/golang.ts
+++ src/data/golang.ts
@@ -2,6 +2,7 @@
 
 import { Sentences } from '../models/types';
 import {
+    END_OF_LINE,
   FIRST_SENTENCE_ID,
 } from '../models/constants';
 
@@ -9,14 +10,56 @@ const typeassertion: Sentences = {
     id: 'type-assertion-id',
     sentences: [
         {
-            id: 'level00-sen-00',
+            id: 'main-defenition',
             positions: {
                 0: {
                     after: FIRST_SENTENCE_ID,
                 },
             },
-            content: 'first sentence'
+            content: 'A type assertion provides access to an interface value\'s underlying concrete value.'
         },
+        {
+            id: 'main-defenition-eol',
+            positions: {
+                0: { after: 'main-defenition' },
+            },
+            content: END_OF_LINE,
+        },
+        {
+            id: 'first-example',
+            positions: {
+                0: { after: 'main-defenition-eol' },
+            },
+            content: '<pre>t := i.(T)</pre>',
+        },
+        {
+            id: 'first-example-eol',
+            positions: {
+                0: { after: 'first-example' },
+            },
+            content: END_OF_LINE,
+        },
+        {
+            id: 'assertion-succeeded',
+            positions: {
+                0: { after: 'first-example-eol' },
+            },
+            content: 'To test whether an interface value holds a specific type, a type assertion can return two values',
+        },
+        {
+            id: 'assertion-succeeded-eol',
+            positions: {
+                0: { after: 'assertion-succeeded' },
+            },
+            content: END_OF_LINE,
+        },
+        {
+            id: 'second-example',
+            content: '<pre>t, ok := i.(T)</pre>',
+            positions: {
+                0: { after: 'assertion-succeeded-eol' },
+            },
+        }
     ],
 };
 

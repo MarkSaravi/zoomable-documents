66ad0403f6773285b8e5f160c59bb2a4c4713788
msaravi <msaravi@nine.com.au>
2020-11-28 09:59:03 +1100

    first example level 2

--- src/data/golang.ts
+++ src/data/golang.ts
@@ -6,15 +6,68 @@ import {
   FIRST_SENTENCE_ID,
 } from '../models/constants';
 
+const firstexample: Sentences = {
+    id: 'first-example-zoomable',
+    sentences: [
+        {
+            id: 'first-example',
+            positions: {
+                0: { after: FIRST_SENTENCE_ID },
+            },
+            content:
+`<pre>
+    t := i.(T)
+</pre>`,
+        },
+        {
+            id: 'first-example-zoom-1',
+            positions: {
+                1: { after: FIRST_SENTENCE_ID },
+            },
+            content:
+`<pre>
+    var i interface{} = "hello"
+
+    s := i.(string)
+    fmt.Println(s)
+        
+    f := i.(float64) // panic
+    fmt.Println(f)
+</pre>`,
+        },
+        {
+            id: 'first-example-zoom-1',
+            positions: {
+                2: { after: FIRST_SENTENCE_ID },
+            },
+            content:
+`<pre>
+    package main
+
+    import "fmt"
+
+    func main() {
+        var i interface{} = "hello"
+
+        s := i.(string)
+        fmt.Println(s)
+            
+        f := i.(float64) // panic
+        fmt.Println(f)
+    }
+</pre>`,
+        },
+    ],
+};
+
 const typeassertion: Sentences = {
     id: 'type-assertion-id',
     sentences: [
         {
             id: 'main-defenition',
             positions: {
-                0: {
-                    after: FIRST_SENTENCE_ID,
-                },
+                0: { after: FIRST_SENTENCE_ID },
+                1: { after: FIRST_SENTENCE_ID },
             },
             content: 'A type assertion provides access to an interface value\'s underlying concrete value.'
         },
@@ -22,27 +75,38 @@ const typeassertion: Sentences = {
             id: 'main-defenition-eol',
             positions: {
                 0: { after: 'main-defenition' },
+                1: { after: 'main-defenition' },
             },
             content: END_OF_LINE,
         },
         {
-            id: 'first-example',
+            id: 'first-example-obj',
             positions: {
                 0: { after: 'main-defenition-eol' },
+                1: { after: 'main-defenition-eol' },
             },
-            content: '<pre>t := i.(T)</pre>',
+            content: firstexample,
         },
         {
-            id: 'first-example-eol',
+            id: 'panic-without-second-param',
+            content: 'This statement asserts that the interface value i holds the concrete type T and assigns the underlying T value to the variable t. If i does not hold a T, the statement will trigger a panic. Zoom above example to see panic and full program.',
             positions: {
-                0: { after: 'first-example' },
+                1: {
+                    after: 'first-example-obj',
+                },
+            }
+        },
+        {
+            id: 'first-example-obj-eol',
+            positions: {
+                0: { after: 'first-example-obj' },
             },
             content: END_OF_LINE,
         },
         {
             id: 'assertion-succeeded',
             positions: {
-                0: { after: 'first-example-eol' },
+                0: { after: 'first-example-obj-eol' },
             },
             content: 'To test whether an interface value holds a specific type, a type assertion can return two values',
         },

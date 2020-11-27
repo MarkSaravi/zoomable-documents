81add2b43104f11d9a0d3714153ac64d3ac221c4
msaravi <msaravi@nine.com.au>
2020-11-28 10:42:03 +1100

    second example

--- src/data/golang.ts
+++ src/data/golang.ts
@@ -17,9 +17,9 @@ const firstexample: Sentences = {
                 0: { after: FIRST_SENTENCE_ID },
             },
             content:
-`${PRE}
+                `${PRE}
     t := i.(T)
-</pre>`,
+                </pre>`,
         },
         {
             id: 'first-example-zoom-1',
@@ -27,7 +27,7 @@ const firstexample: Sentences = {
                 1: { after: FIRST_SENTENCE_ID },
             },
             content:
-`${PRE}
+                `${PRE}
     var i interface{} = "hello"
 
     s := i.(string)
@@ -35,15 +35,15 @@ const firstexample: Sentences = {
         
     f := i.(float64) // panic
     fmt.Println(f)
-</pre>`,
+                </pre>`,
         },
         {
-            id: 'first-example-zoom-1',
+            id: 'first-example-zoom-2',
             positions: {
                 2: { after: FIRST_SENTENCE_ID },
             },
             content:
-`${PRE}
+                `${PRE}
     package main
 
     import "fmt"
@@ -57,7 +57,61 @@ const firstexample: Sentences = {
         f := i.(float64) // panic
         fmt.Println(f)
     }
-</pre>`,
+                </pre>`,
+        },
+    ],
+};
+
+const secondexample: Sentences = {
+    id: 'second-example-zoomable',
+    sentences: [
+        {
+            id: 'second-example',
+            positions: {
+                0: { after: FIRST_SENTENCE_ID },
+            },
+            content:
+                `${PRE}
+    t, ok := i.(T)
+                </pre>`,
+        },
+        {
+            id: 'second-example-zoom-1',
+            positions: {
+                1: { after: FIRST_SENTENCE_ID },
+            },
+            content:
+                `${PRE}
+    var i interface{} = "hello"
+
+    s, ok := i.(string)
+    fmt.Println(s, ok)
+
+    f, ok := i.(float64)
+    fmt.Println(f, ok)
+                </pre>`,
+        },
+        {
+            id: 'second-example-zoom-2',
+            positions: {
+                2: { after: FIRST_SENTENCE_ID },
+            },
+            content:
+                `${PRE}
+    package main
+
+    import "fmt"
+
+    func main() {
+        var i interface{} = "hello"
+
+        s, ok := i.(string)
+        fmt.Println(s, ok)
+    
+        f, ok := i.(float64)
+        fmt.Println(f, ok)
+    }
+                </pre>`,
         },
     ],
 };
@@ -110,20 +164,30 @@ const typeassertion: Sentences = {
             positions: {
                 0: { after: 'first-example-obj-eol' },
             },
-            content: 'If the interface does not hold the specific type it triggers panic. To avoid panic we can use the following pattern.',
+            content: 'If the interface does not hold the specific type it triggers panic.',
+        },
+        {
+            id: 'assertion-succeeded-2',
+            positions: {
+                0: { after: 'assertion-succeeded' },
+                1: { after: 'panic-without-second-param' },
+            },
+            content: ' To avoid panic we can use the following pattern.',
         },
         {
             id: 'assertion-succeeded-eol',
             positions: {
-                0: { after: 'assertion-succeeded' },
+                0: { after: 'assertion-succeeded-2' },
+                1: { after: 'assertion-succeeded-2' },
             },
             content: END_OF_LINE,
         },
         {
             id: 'second-example',
-            content: '<pre>t, ok := i.(T)</pre>',
+            content: secondexample,
             positions: {
                 0: { after: 'assertion-succeeded-eol' },
+                1: { after: 'assertion-succeeded-eol' },
             },
         }
     ],

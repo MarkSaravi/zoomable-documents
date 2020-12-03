7510d30fc73fc35aa2810931870e0290abc46a5c
msaravi <msaravi@nine.com.au>
2020-12-04 07:49:51 +1100

    adding ww2

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,17 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import "./App.css";
 import Zoomable from "./components/Zooamble";
 import ferdowsi from './data/Ferdowsi';
-import colors from './data/color-tests';
-import news00 from './data/news00';
-import news01 from './data/news01';
-import news02 from './data/news02';
 import golang from './data/golang';
-import logo from './data/logo';
+import ww2 from './data/ww2';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Switch>
-          <Route path="/news/0" component={() => (<Zoomable sentences={news00} />)} />
-          <Route path="/news/1" component={() => (<Zoomable sentences={news01} />)} />
-          <Route path="/news/2" component={() => (<Zoomable sentences={news02} />)} />
-          <Route path="/colors" component={() => (<Zoomable sentences={colors} />)} />
           <Route path="/golang" component={() => (<Zoomable sentences={golang} />)} />
           <Route path="/farsi" component={() => (<Zoomable sentences={ferdowsi} />)} />
-          <Route path="/logo" component={() => (<Zoomable sentences={logo} />)} />
+          <Route path="/ww2" component={() => (<Zoomable sentences={ww2} />)} />
         </Switch>
       </BrowserRouter>
     </div>
---
+++ src/data/ww2.ts
@@ -0,0 +1,159 @@
+/* eslint-disable import/extensions */
+
+import { Sentences } from '../models/types';
+import {
+  FIRST_SENTENCE_ID,
+} from '../models/constants';
+
+const DIVIDER = '<div style="width: 100%; border-bottom: solid 2px black; margin: 16px"/>';
+
+const strtimeline: Sentences = {
+    id: 'strtm',
+    sentences: [
+        {
+            id: 'strtm-0',
+            content: '<h4>Dec 1941<h4>',
+            positions: {
+                0: { after: FIRST_SENTENCE_ID },
+            }
+        },
+        {
+            id: 'strtm-1',
+            content: 'On 7 December 1941, Japanese forces attacked the United States Pacific Fleet at Naval Station Pearl Harbor, incorporated territory of Hawaii.',
+            positions: {
+                0: { after: 'strtm-0' }
+            }
+        },
+        {
+            id: 'strtm-2',
+            content: '<h4>May 1942<h4>',
+            positions: {
+                1: { after: FIRST_SENTENCE_ID },
+            }
+        },
+        {
+            id: 'strtm-3',
+            content: 'The Japanese made two attempts to continue their strategic initiative, and offensively extend their outer defensive perimeter in the south and central Pacific to where they could threaten Australia and Hawaii or the U.S. West Coast.',
+            positions: {
+                1: { after: 'strtm-2' }
+            }
+        },
+    ]
+};
+
+const strategicconsiderations: Sentences = {
+    id: 'str',
+    sentences: [
+        {
+            id: 'str-0',
+            content: DIVIDER,
+            positions: {
+                0: { after: FIRST_SENTENCE_ID }
+            }
+        },
+        {
+            id: 'str-1',
+            content: '<h3>Strategic considerations</h3>',
+            positions: {
+                0: { after: 'str-0' }
+            }
+        },
+        {
+            id: 'str-2',
+            content: strtimeline,
+            positions: {
+                0: { after: 'str-1' },
+            },
+        },
+    ]
+};
+
+const taskforcetimeline: Sentences = {
+    id: 'tftl',
+    sentences: [
+        {
+            id: 'tftl-0',
+            content: '<h4>May 1942<h4>',
+            positions: {
+                0: { after: FIRST_SENTENCE_ID },
+            }
+        },
+        {
+            id: 'tftl-1',
+            content: 'In preparation for the offensive in the Pacific in May 1942, U.S. Marine Major General Alexander Vandegrift was ordered to move his 1st Marine Division from the United States to New Zealand.',
+            positions: {
+                0: { after: 'tftl-0' }
+            }
+        },
+        {
+            id: 'tftl-2',
+            content: '<h4>Jul 1942<h4>',
+            positions: {
+                1: { after: FIRST_SENTENCE_ID },
+            }
+        },
+        {
+            id: 'tftl-3',
+            content: 'The Watchtower force, numbering 75 warships and transports (of vessels from the U.S. and Australia), assembled near Fiji on 26 July and engaged in one rehearsal landing prior to leaving for Guadalcanal on 31 July.',
+            positions: {
+                1: { after: 'tftl-2' }
+            }
+        },
+    ]
+};
+
+const taskforce: Sentences = {
+    id: 'tf',
+    sentences: [
+        {
+            id: 'tf-0',
+            content: DIVIDER,
+            positions: {
+                0: { after: FIRST_SENTENCE_ID }
+            }
+        },
+        {
+            id: 'tf-1',
+            content: '<h3>Task force</h3>',
+            positions: {
+                0: { after: 'tf-0' }
+            }
+        },
+        {
+            id: 'tf-2',
+            content: taskforcetimeline,
+            positions: {
+                0: { after: 'tf-1' },
+            },
+        },
+    ]
+};
+
+const timeline: Sentences = {
+    id: 'guadalcanal',
+    sentences: [
+        {
+            id: 'gua-title',
+            content: '<h2>Guadalcanal campaign</h2>',
+            positions: {
+                0: { after: FIRST_SENTENCE_ID },
+            },
+        },
+        {
+            id: 'str-con',
+            content: strategicconsiderations,
+            positions: {
+                0: { after: 'gua-title' },
+            },
+        },
+        {
+            id: 'str-tf',
+            content: taskforce,
+            positions: {
+                0: { after: 'str-con' },
+            },
+        },
+    ],
+};
+
+export default timeline;
